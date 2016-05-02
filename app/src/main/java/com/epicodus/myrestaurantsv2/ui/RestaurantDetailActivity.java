fcd4b2081434df3368b3b960d78eb6288b632466
Megan <mefayer@gmail.com>
2016-05-02 09:15:03 -0700

    finish page adapter

--- app/src/main/java/com/epicodus/myrestaurantsv2/ui/RestaurantDetailActivity.java
+++ app/src/main/java/com/epicodus/myrestaurantsv2/ui/RestaurantDetailActivity.java
@@ -1,15 +1,34 @@
 package com.epicodus.myrestaurantsv2.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.myrestaurantsv2.R;
+import com.epicodus.myrestaurantsv2.adapters.RestaurantPagerAdapter;
+import com.epicodus.myrestaurantsv2.models.Restaurant;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
+    @Bind(R.id.viewPager) ViewPager mViewPager;
+    private RestaurantPagerAdapter adapterPageView;
+    ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+        ButterKnife.bind(this);
+        mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        int startingPosition = Integer.parseInt(getIntent().getStringExtra("position"));
+        adapterPageView = new RestaurantPagerAdapter(getSupportFragmentManager(), mRestaurants);
+        mViewPager.setAdapter(adapterPageView);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
